b6351e393742d466d217478f1f4d802b139b388b
Bongx514 <bonganithwala514@live.com>
2026-08-18 08:31:29 +0200

    Getting project repo ready for development

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarServiceManager.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
